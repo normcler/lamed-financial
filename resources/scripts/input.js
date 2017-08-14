5b4eaa2f4da5cecce73e6bfe7da6c6ac9cd8090e
normcler <norm.clerman@gmail.com>
2017-08-14 15:37:46 -0500

    rewritting using AngularJS

--- resources/scripts/input.js
+++ resources/scripts/input.js
@@ -4,6 +4,27 @@
     append('<span class="td"><input type="number"/></span>');
 }*/
 
+( function() {
+    "use strict";
+
+    angular.module("input", [])
+
+    angular.module("input").
+        controller("inputController", function($scope) {
+        });
+    // This is a custom directive that I've defined, not something built into
+    // angular. I can stitch the div into my code by putting <my-directive>
+
+    angular.module("input").
+        directive("myDirective",function () {
+            return {
+                template: "<div>This is a custom directive</div>"
+            }
+        });
+})();
+
+
+
 var populateTable = function () {
     var inputForm = $(".input-form")
     for (var jRow = 0; jRow < 25; jRow++) {
